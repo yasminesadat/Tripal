5861efaab584900f85a6f225c44e2132106b631e
nabilasherif <nabila.sherif81@gmail.com>
2024-09-27 14:01:24 +0300

    removed id from the datamodels of seller, advertiser and tourguide

--- backend/models/Seller.js
+++ backend/models/Seller.js
@@ -1,26 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const { v4: uuidv4 } = require('uuid');
 
 const sellerSchema = new Schema({
-  userName: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  accepted:{
-    type: Boolean,
-    default: false,
-  }
+    userName: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    accepted:{
+        type: Boolean,
+        default: false,
+    }
 }, { timestamps: true });
 
 const Seller = mongoose.model('Seller', sellerSchema);
--- backend/models/TourGuide.js
+++ backend/models/TourGuide.js
@@ -1,13 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const { v4: uuidv4 } = require('uuid');
 
 const tourGuideSchema = new Schema({
-    _id: {
-        type: String,
-        default: uuidv4,
-        unique : true
-    },
     userName: {
         type: String,
         required: true,
