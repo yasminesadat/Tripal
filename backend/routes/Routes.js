1ebe7c9db417a558d342662f5a41e6a661cd090f
diana <daiana.rehan.dr@gmail.com>
2024-09-27 14:22:26 +0300

    changed routes/controller to camelCase

--- backend/routes/Routes.js
+++ backend/routes/Routes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { createTourGuide } = require('../controllers/TourGuideController.js');
-const { createSeller } = require('../controllers/SellerController.js');
-const { createAdvertiser } = require('../controllers/AdvertiserController.js');
+const { createTourGuide } = require('../controllers/tourGuideController.js');
+const { createSeller } = require('../controllers/sellerController.js');
+const { createAdvertiser } = require('../controllers/advertiserController.js');
 
-// defining tourguide routes
-router.post("/tourguide",createTourGuide);
+// defining tour-guide routes
+router.post("/tour-guide",createTourGuide);
 
 // defining seller routes
 router.post("/seller",createSeller);
