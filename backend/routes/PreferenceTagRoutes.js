34a7708987f64dc14130e358e344eb95cf6bf77c
hanaseif19 <hana.seiff19@gmail.com>
2024-09-29 02:28:12 +0300

    pref-Tags change paths

--- backend/routes/PreferenceTagRoutes.js
+++ backend/routes/PreferenceTagRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getPrefTags, createPrefTags, updatePrefTags, deletePrefTags } = require('../controllers/preferenceTagController');
-router.get('/getPrefTags', getPrefTags);
-router.post('/createPrefTags', createPrefTags);
-router.put('/updatePrefTags/:id', updatePrefTags);
-router.delete('/deletePrefTags/:id', deletePrefTags);
+const { getPrefTags, createPrefTags, updatePrefTags, deletePrefTags } = require('../controllers/PreferenceTagController');
+
+router.get('/pref-tags', getPrefTags);
+router.post('/pref-tags', createPrefTags);
+router.put('/pref-tags/:id', updatePrefTags);
+router.delete('/pref-tags/:id', deletePrefTags);
 
 module.exports = router;
