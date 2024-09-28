233589067b8cd791332cb080ad31f048988b0ae3
hanaseif19 <hana.seiff19@gmail.com>
2024-09-29 02:04:27 +0300

    fixed file names

--- backend/controllers/preferenceTagController.js
+++ backend/controllers/preferenceTagController.js
@@ -1,72 +1,72 @@
-const preferenceTag = require ('../models/PreferenceTag.js');
+const preferenceTag = require('../models/PreferenceTag.js');
 const { default: mongoose } = require('mongoose');
 
-const createPrefTags =  async (req, res) => {
-    const {Name} = req.body;
+const createPrefTags = async (req, res) => {
+    const { Name } = req.body;
     try {
-        const createdPrefTag = await preferenceTag.create ({Name});
+        const createdPrefTag = await preferenceTag.create({ Name });
 
-        if (!Name){
-            res.status (406).json ('Name is required');
+        if (!Name) {
+            res.status(406).json('Name is required');
         }
-        res.status (200).json ({status: 'success', data: {createdPrefTag}});
+        res.status(200).json({ status: 'success', data: { createdPrefTag } });
     }
-    catch (error){
-        res.status (400).json ({error:error.message});
+    catch (error) {
+        res.status(400).json({ error: error.message });
     }
 }
 
 const getPrefTags = async (req, res) => {
-    try{
+    try {
         const tags = await preferenceTag.find();
 
         if (tags.length === 0) {
             return res.status(404).json('No tags found');
         }
 
-        res.status (200).json (tags);
+        res.status(200).json(tags);
     }
-    catch (error){
-        res.status (400).json ('Error fetching all tags', error);
+    catch (error) {
+        res.status(400).json('Error fetching all tags', error);
     }
 }
 
 const updatePrefTags = async (req, res) => {
-    try{
-        const {id} = req.params;
-        const {Name} = req.body;
+    try {
+        const { id } = req.params;
+        const { Name } = req.body;
 
         const updatedPrefTag = await preferenceTag.findByIdAndUpdate(
             id,
-            {Name},
-            {new:true});
+            { Name },
+            { new: true });
 
-        if (!updatedPrefTag){
-            return res.status (404).json ('Tag does not exist');
+        if (!updatedPrefTag) {
+            return res.status(404).json('Tag does not exist');
         }
 
-        return res.status (200).json ({status: 'success', data: {updatedPrefTag}})
+        return res.status(200).json({ status: 'success', data: { updatedPrefTag } })
     }
-    catch (error){
-        res.status (400).json ({error: error.message});
+    catch (error) {
+        res.status(400).json({ error: error.message });
     }
 }
 
 
 const deletePrefTags = async (req, res) => {
-    const {id} = req.params;
-    try{
+    const { id } = req.params;
+    try {
         const deletedPrefTag = await preferenceTag.findByIdAndDelete(id);
 
-        if (!deletedPrefTag){
-            return res.status (404).json ('Tag does not exist');
+        if (!deletedPrefTag) {
+            return res.status(404).json('Tag does not exist');
         }
 
-        return res.status (200).json ('Tag deleted successfully')
+        return res.status(200).json('Tag deleted successfully')
     }
-    catch (error){
-        res.status (400).json ('Error deleting tag', error);
+    catch (error) {
+        res.status(400).json('Error deleting tag', error);
     }
 }
 
-module.exports = {createPrefTags, getPrefTags, updatePrefTags, deletePrefTags};
+module.exports = { createPrefTags, getPrefTags, updatePrefTags, deletePrefTags };
--- backend/routes/preferenceTagRoutes.js
+++ backend/routes/preferenceTagRoutes.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {getPrefTags, createPrefTags, updatePrefTags, deletePrefTags} = require ('../controllers/PreferenceTagController');
-
-router.get ('/getPrefTags', getPrefTags);
-router.post ('/createPrefTags', createPrefTags);
-router.put ('/updatePrefTags/:id', updatePrefTags);
-router.delete ('/deletePrefTags/:id', deletePrefTags);
+const { getPrefTags, createPrefTags, updatePrefTags, deletePrefTags } = require('../controllers/preferenceTagController');
+router.get('/getPrefTags', getPrefTags);
+router.post('/createPrefTags', createPrefTags);
+router.put('/updatePrefTags/:id', updatePrefTags);
+router.delete('/deletePrefTags/:id', deletePrefTags);
 
 module.exports = router;
