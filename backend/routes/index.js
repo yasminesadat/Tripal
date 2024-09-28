e059317421c74473b9e4cc1674cbda8898e56e58
hanaseif19 <hana.seiff19@gmail.com>
2024-09-28 22:20:14 +0300

    Updated index.js to fix routes to start with /api

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
-
-// Create a new router
 const router = express.Router();
 
-// Use the imported routes
-router.use('/prefTags', require('./PreferenceTagRoutes'));
-router.use('/sellers', require('./SellerRoutes'));
-router.use('/tourguides', require('./TourGuideRoutes'));
-router.use('/advertisers', require('./AdvertiserRoutes'));
-router.use('/tourist', require('./touristRoutes'));
-router.use('/activityCategory', require('./activityControllerRoutes'));
+const routes = [
+    './PreferenceTagRoutes',
+    './SellerRoutes',
+    './TourGuideRoutes',
+    './AdvertiserRoutes',
+    './activityControllerRoutes'
+];
+
+routes.forEach(route => {
+    router.use('/api', require(route));
+});
+
 module.exports = router;
