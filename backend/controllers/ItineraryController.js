a6b154c97a47fc3b2e442175007eb9f884053422
diana <daiana.rehan.dr@gmail.com>
2024-10-01 00:25:32 +0300

    wrote some confusing comments at the model

---
+++ backend/controllers/ItineraryController.js
@@ -0,0 +1,8 @@
+const itineraryController = require('..models/Itinerary.js');
+
+const createItinerary = async(req,res) => {};
+const getItineraries = async(req,res) => {};
+const updateItinerary = async(req,res) => {};
+const deleteItinerary = async(req,res) => {};
+
+module.exports = {createItinerary, getItineraries, updateItinerary, deleteItinerary};
--- backend/models/Itinerary.js
+++ backend/models/Itinerary.js
@@ -4,13 +4,14 @@ const itinerarySchema = new mongoose.Schema({
     activities:[{type: mongoose.Schema.Types.ObjectId, ref: 'Activity'
         , required: true}],//this is data contained, how to access the data ??
     locations:[{type:String, required: true}],
-    //timeline
-    //duration for each activity
+    //timeline btetrateb??
+    //timeline: {type: String,required: true,},
+    //duration for each activity activity 3ando time already
     language: {type: String, required: true},
-    price:{type: Number, required: true},
+    price:{type: Number, required: true}, //activity 3ando price range
     availableDates:[{type: Date, required: true}],
     availableTime: [{type: String, required: true}],
-    accessability: {type: String, required: true},
+    accessibility: {type: String, required: true},
     pickupLocation: {type: String, required: true},
     dropoffLocation: {type: String, required: true},
     //make a fcuntion that allow deletion or not maybe a booking schema 
