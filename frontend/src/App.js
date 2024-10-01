7cfe61cfbf1a5b17b970b5026e6f0a28f0aa8eaa
Mariam0Gafar <mariam.gaafar02@gmail.com>
2024-10-02 00:29:43 +0300

    products details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import * as Pages from './pages';
 import TagManager from './components/PreferenceTagComponent';
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <div className='pages'>
+        <div className="pages">
           <Routes>
             <Route path="/" element={<Pages.Home />} />
             <Route path="/preference-tags" element={<TagManager />} />
             <Route path="/view-products" element={<Pages.ProductList />} />
+            <Route path="/product/:productName" element={<Pages.ProductDetails />} />
           </Routes>
         </div>
       </BrowserRouter>
-
     </div>
-    
   );
 }
 
