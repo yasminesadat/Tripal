368b563cb632cde1e14f5a0591540d220b149763
hanaseif19 <hana.seiff19@gmail.com>
2024-10-02 00:40:44 +0300

    rename apis in activity category

--- backend/controllers/ActivityCategoryController.js
+++ backend/controllers/ActivityCategoryController.js
@@ -1,26 +1,69 @@
-const activityCategory = require("../models/ActivityCategory");
+const activityCategory = require('../models/ActivityCategory')
+const { default: mongoose } = require('mongoose');
+
 
 const createActivityCategory = async (req, res) => {
-  try {
-    const activityCategoryCreated = await activityCategory.create({
-      Name: req.body.Name,
-    });
-    res.status(200).json(activityCategoryCreated);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-};
+    try {
+        const activityCategoryCreated = await activityCategory.create({
+            Name: req.body.Name
+        })
+        res.status(200).json(activityCategoryCreated)
+    }
+    catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 
+}
 const getActivityCategories = async (req, res) => {
-  try {
-    const existingActivityCategories = await activityCategory.find({});
-    res.status(200).json(existingActivityCategories);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
+    try {
+        const existingActivityCategories = await activityCategory.find({})
+        res.status(200).json(existingActivityCategories)
+    }
+    catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+
+}
+const deleteActivityCategory = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deletedCategory = await activityCategory.findByIdAndDelete(id);
+
+        if (!deletedCategory) {
+            return res.status(404).json({ message: "Activity category not found" });
+        }
+
+        res.status(200).json({ message: "Activity category deleted successfully", data: { deletedCategory } });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 };
+const updateActivityCategory = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { Name } = req.body;
+
+        const newActivityCategory = await activityCategory.findByIdAndUpdate(
+            id,
+            { Name },
+            { new: true });
+
+        if (!newActivityCategory) {
+            return res.status(404).json('Activity category not found');
+        }
+
+        return res.status(200).json({ status: 'success', data: { newActivityCategory } })
+    }
+    catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+}
+
 
 module.exports = {
-  createActivityCategory,
-  getActivityCategories,
-};
+    createActivityCategory,
+    getActivityCategories,
+    deleteActivityCategory,
+    updateActivityCategory
+
+}
--- backend/models/Tourist.js
+++ backend/models/Tourist.js
@@ -2,39 +2,39 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const touristSchema = new Schema(
-  {
-    userName: {
-      type: String,
-      required: true,
-      unique: true,
+    {
+        userName: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        mobileNumber: {
+            type: String,
+            required: true,
+        },
+        nationality: {
+            type: String,
+            required: true,
+        },
+        dateOfBirth: {
+            type: Date,
+            required: true,
+        },
+        job: {
+            type: String,
+            required: true,
+        },
     },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    mobileNumber: {
-      type: String,
-      required: true,
-    },
-    nationality: {
-      type: String,
-      required: true,
-    },
-    dateOfBirth: {
-      type: Date,
-      required: true,
-    },
-    job: {
-      type: String,
-      required: true,
-    },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
 module.exports = mongoose.model("Tourist", touristSchema);
