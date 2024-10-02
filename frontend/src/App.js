4ab8cf63cb41e456b9cb00fe7303fca60a01d13c
Mennah Shaker <mennahmohamedshaker0@gmail.com>
2024-10-02 16:34:09 +0300

    updated bug issue and added admin dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import * as Pages from './pages';
 import TagManager from './components/PreferenceTagComponent';
+import AdminDashboard from './components/AdminDashboard';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <div className="pages">
           <Routes>
             <Route path="/" element={<Pages.Home />} />
+            <Route path="/admin" element={<AdminDashboard />} />
             <Route path="/preference-tags" element={<TagManager />} />
             <Route path="/view-products" element={<Pages.ProductList />} />
             <Route path="/product/:productName" element={<Pages.ProductDetails />} />
