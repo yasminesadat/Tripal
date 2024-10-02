7db873a75b7ced2c074cea2b4f10bcded6ebb77f
hanaseif19 <hana.seiff19@gmail.com>
2024-10-02 15:29:54 +0300

    updated user schemas (missing update and delete methods)

--- backend/controllers/TourGuideController.js
+++ backend/controllers/TourGuideController.js
@@ -1,18 +1,44 @@
-const tourGuideModel=require('../models/TourGuide.js');
+const tourGuideModel = require('../models/TourGuide.js');
 const bcrypt = require('bcrypt');
+const userModel = require('../models/User.js')
 
-const createTourGuide = async(req,res) => {
-    try{
+const createTourGuide = async (req, res) => {
+    try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        const tourGuide = await tourGuideModel.create({ 
-            userName: req.body.userName,  
+        const tourGuide = await tourGuideModel.create({
+            userName: req.body.userName,
             email: req.body.email,
             password: hashedPassword
         });
+        const id = tourGuide._id
+        await userModel.create({
+            userID: id,
+            role: "TourGuide"
+        })
         res.status(200).json(tourGuide);
-    }catch (error) {
-       res.status(400).json({ error: error.message });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
     }
 }
+const updateTourguideData = async (req, res) => {
+    try {
+        const { id } = req.params;
 
-module.exports = {createTourGuide};
+
+
+        const updatedTourGuide = await tourGuideModel.findByIdAndUpdate(
+            id,
+            { ...req.body },
+            { new: true });
+
+        if (!updatedTourGuide) {
+            return res.status(404).json('Tour Guide not found');
+        }
+
+        return res.status(200).json({ updatedTourGuide })
+    }
+    catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+}
+module.exports = { createTourGuide, updateTourguideData };
--- backend/routes/SellerRoutes.js
+++ backend/routes/SellerRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {createSeller} = require ('../controllers/SellerController');
+const { createSeller, updateSellerData } = require('../controllers/SellerController');
 
 // defining seller routes
 router.post("/seller", createSeller);
+router.put("/seller/:id", updateSellerData);
 
 module.exports = router;
