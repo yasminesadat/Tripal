9dda0b001a5416a91053b12b72a7af1d8e4a5cfe
diana <daiana.rehan.dr@gmail.com>
2024-10-02 19:57:16 +0300

    Update method done (not tested)

--- backend/controllers/ItineraryController.js
+++ backend/controllers/ItineraryController.js
@@ -56,7 +56,48 @@ const createItinerary = async(req,res) => {
     };
 
 const getItineraries = async(req,res) => {};
-const updateItinerary = async(req,res) => {};
+
+const updateItinerary = async(req,res) => {
+    try{
+        const {id} = req.params;
+        const {title, description, tourGuide, activities,
+            language,availableDates,availableTime,accessibility,
+            pickupLocation,dropoffLocation} = req.body;
+        
+        const fetchedActivities = await activityModel.find({ _id: { $in: activities } });
+        
+        if (!fetchedActivities || fetchedActivities.length === 0) {
+            return res.status(404).json({ error: 'No activities found' });
+        }
+
+        let price = 0;
+        const locations = [];
+        const timeline = [];
+        const allTags = new Set();
+
+        fetchedActivities.forEach((activity) => {
+            price+=activity.price;
+            locations.push(activity.location);
+            timeline.push({activityName: activity.title, time: activity.time});
+            activity.tags.forEach((tag) => allTags.add(tag));
+        })
+
+        const uniqueTags = Array.from(allTags);
+
+        const updatedItinerary = await itineraryModel.findByIdAndUpdate(id, {title, description, 
+            tourGuide, activities, availableDates, availableTime, language,
+            accessibility, pickupLocation, dropoffLocation, price, locations,
+            timeline,tags: uniqueTags},{ new: true });
+        
+            if (!updatedItinerary) {
+                return res.status(404).json({ error: 'Itinerary not found' });
+            }
+            res.status(200).json(updatedItinerary);
+    }
+    catch(error){
+        res.status(400).json({ error: error.message });
+    }
+};
 
 
 const deleteItinerary = async(req,res) => {
