4bd2ac533fac5548d30a6e9456171d684a72838e
hanaseif19 <hana.seiff19@gmail.com>
2024-10-05 01:34:26 +0300

    styling to tourist profile

--- frontend/src/components/TouristProfile.js
+++ frontend/src/components/TouristProfile.js
@@ -2,18 +2,16 @@ import React from "react";
 
 const TouristProfile = ({ tourist }) => {
   return (
-    <ul>
+    <ul className="tourist-profile">
       <li key={tourist._id}>
         <h2>{tourist.userName}</h2>
-        <p>Email: {tourist.email}</p>
-        <p>
-          Date of birth: {new Date(tourist.dateOfBirth).toLocaleDateString()}
-        </p>
-        <p>Nationality: {tourist.nationality}</p>
-        <p>Job: {tourist.job}</p>
-        <p>Mobile Number: {tourist.mobileNumber}</p>
-      </li>
-    </ul>
+        <p><b>Email:</b> {tourist.email}</p>
+        <p><b>Date of birth:</b> {new Date(tourist.dateOfBirth).toLocaleDateString()}</p>
+        <p><b>Nationality:</b>{tourist.nationality}</p>
+        <p><b>Job: </b>{tourist.job}</p>
+        <p><b>Mobile Number: </b>{tourist.mobileNumber}</p>
+      </li >
+    </ul >
   );
 };
 
