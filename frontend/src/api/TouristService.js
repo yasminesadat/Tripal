46678ecff9787021c971cd8e6a2cad9e441b7bad
Yasmine Elsadat <yasmine.atef2004@gmail.com>
2024-10-04 20:26:32 +0300

    formatUsingPrettier

--- frontend/src/api/TouristService.js
+++ frontend/src/api/TouristService.js
@@ -1,31 +1,29 @@
-import { axios } from './axios'
+import { axios } from "./axios";
 
 export async function createTourist(newUser) {
-    try {
-        console.log("the user", newUser, "end of user")
-        await axios.post('/createTourist', newUser);
-    } catch (error) {
-        console.log("eee", error);
-        throw error;
-    }
+  try {
+    console.log("the user", newUser, "end of user");
+    await axios.post("/createTourist", newUser);
+  } catch (error) {
+    console.log("eee", error);
+    throw error;
+  }
 }
 export async function getTouristInformation(id) {
-    try {
-
-        const response = await axios.get(`/getTouristInfo/${id}`);
-        return response.data
-    } catch (error) {
-        console.log("eee", error);
-        throw error;
-    }
+  try {
+    const response = await axios.get(`/getTouristInfo/${id}`);
+    return response.data;
+  } catch (error) {
+    console.log("eee", error);
+    throw error;
+  }
 }
 export async function updateTouristInformation(id) {
-    try {
-
-        const response = await axios.put(`/updateTourist/${id}`);
-        return response.data
-    } catch (error) {
-        console.log("eee", error);
-        throw error;
-    }
+  try {
+    const response = await axios.put(`/updateTourist/${id}`);
+    return response.data;
+  } catch (error) {
+    console.log("eee", error);
+    throw error;
+  }
 }
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,20 +1,25 @@
-import React from 'react';
-import SignUp from '../components/SignUp';
-import SignUpAllUsers from '../components/SignUpAllUsers'
-import mainLogo from '../assets/images/Tripal-logo.png'
+import React from "react";
+import SignUp from "../components/SignUp";
+import SignUpAllUsers from "../components/SignUpAllUsers";
+import mainLogo from "../assets/images/Tripal-logo.png";
 const Home = () => {
-    return (
-        <div className="home">
-            <h1>WELCOME TO TRIPAL</h1>
-            <img
-                src={mainLogo}
-                alt="Description of the image"
-                style={{ float: 'right', maxWidth: '100%', height: 'auto', marginTop: '-15%' }}
-            />
+  return (
+    <div className="home">
+      <h1>WELCOME TO TRIPAL</h1>
+      <img
+        src={mainLogo}
+        alt="Description of the image"
+        style={{
+          float: "right",
+          maxWidth: "100%",
+          height: "auto",
+          marginTop: "-15%",
+        }}
+      />
 
-            <SignUpAllUsers />
-        </div>
-    );
+      <SignUpAllUsers />
+    </div>
+  );
 };
 
 export default Home;
