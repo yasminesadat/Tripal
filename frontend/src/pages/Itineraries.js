36d856a099a03857c96a1c05cb44a502e714877d
diana <daiana.rehan.dr@gmail.com>
2024-10-04 18:15:46 +0300

    navigating to a new page for itinerary actions

---
+++ frontend/src/pages/Itineraries.js
@@ -0,0 +1,16 @@
+import {react} from 'react';
+import { Link } from 'react-router-dom';
+
+
+const ItinerariesPage = () => {
+
+
+    
+    return(
+    <div>
+        <h1>Itineraries</h1>
+        <p>Itineraries page content</p>
+    </div>
+)};
+
+export default ItinerariesPage;
