19c381caf8d540b0a41fecc8599ade955a560c61
Yasmine Elsadat <yasmine.atef2004@gmail.com>
2024-10-07 00:28:46 +0300

    addedEditAutoFill

--- frontend/src/components/seller/ProductCard.js
+++ frontend/src/components/seller/ProductCard.js
@@ -15,27 +15,42 @@ const ProductCard = ({
   quantity,
   picture,
   ratings,
-  averageRating
+  averageRating,
 }) => {
   const navigate = useNavigate();
 
   const handleCardClick = () => {
     navigate(`/product/${id}`, {
-      state: { name, seller, price, description, quantity, picture, ratings, averageRating },
+      state: {
+        name,
+        seller,
+        price,
+        description,
+        quantity,
+        picture,
+        ratings,
+        averageRating,
+      },
     });
   };
-  
+
   const handleEditClick = (e) => {
     navigate(`/edit-product/${id}`, {
-      state: { picture },
+      state: {
+        initialName: name,
+        initialPrice: price,
+        initialDescription: description,
+        initialQuantity: quantity,
+        initialPicture: picture,
+      },
     });
   };
 
   const descriptionLength = description.length;
-    const formattedDescription = (
-    <div style={{ flexDirection: 'column' }}>
+  const formattedDescription = (
+    <div style={{ flexDirection: "column" }}>
       {description}
-      {descriptionLength <= 30 && <div style={{ height: '20px' }} />} 
+      {descriptionLength <= 30 && <div style={{ height: "20px" }} />}
     </div>
   );
 
@@ -54,17 +69,19 @@ const ProductCard = ({
       }
       actions={[
         <EditOutlined key="edit" onClick={handleEditClick} />,
-        <EllipsisOutlined key="ellipsis" onClick={handleCardClick}/>,
+        <EllipsisOutlined key="ellipsis" onClick={handleCardClick} />,
       ]}
     >
       <Meta
         title={`${name} - $${price}`}
-        description={formattedDescription} 
+        description={formattedDescription}
         onClick={handleCardClick}
       />
       <div style={{ marginTop: "20px", textAlign: "center" }}>
         <Rate value={averageRating} disabled allowHalf />
-        <span style={{marginLeft:'5%', marginBottom:'5%', color:"gray"}}>({averageRating})</span>
+        <span style={{ marginLeft: "5%", marginBottom: "5%", color: "gray" }}>
+          ({averageRating})
+        </span>
       </div>
     </Card>
   );
