a7601d71b889fc27a41788b766a4b189394673e6
MernaSenger <mernasenger753@gmail.com>
2024-10-06 04:34:02 +0300

    starting admin user stories

--- frontend/src/components/guest/GuestNavBar.js
+++ frontend/src/components/guest/GuestNavBar.js
@@ -29,6 +29,9 @@ const GuestNavBar = () => {
         <div class="nav-bar-link">
         <Link to="/create-historicalPlace">Historical Places</Link>  
         </div>
+        <div class="nav-bar-link">
+          <Link to="/adminDeleteUser">View User</Link>
+        </div>
       </div>
     </nav>
   );
--- frontend/src/routes/AdminRoutes.js
+++ frontend/src/routes/AdminRoutes.js
@@ -3,6 +3,9 @@ import ActivityCategoryDetails from "../pages/admin/ActivityCategoryDetails";
 import TagManager from "../pages/admin/PreferenceTags";
 import AdminProducts from "../pages/admin/AdminProducts";
 import AdminViewProduct from "../pages/admin/AdminViewProduct";
+import AdminViewUsers from "../components/AdminViewUsers";
+import CreateNewGovernor from "../components/CreateNewGovernor";
+import AdminCreateNew from "../components/AdminCreateNew";
 
 const adminRoutes = [
   { path: "/admin", element: <AdminDashboard /> },
@@ -10,6 +13,9 @@ const adminRoutes = [
   { path: "/preference-tags", element: <TagManager /> },
   { path: "/admin/view-products", element: <AdminProducts /> },
   { path: "/admin/view-products/product/:id", element: <AdminViewProduct /> },
+  { path: "/adminDeleteUser", element: <AdminViewUsers /> },
+  { path: "/new-admin", element: <AdminCreateNew /> },
+  { path: "/new-governor", element: <CreateNewGovernor /> }
 ];
 
 export default adminRoutes;
