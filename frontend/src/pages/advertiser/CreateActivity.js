bcbf4a764e9c5a2c1411e3ab886dee1df9662b74
janasaad7 <janaahmedsaad@gmail.com>
2024-10-07 00:05:02 +0300

    removed id param from navbar

--- frontend/src/pages/advertiser/CreateActivity.js
+++ frontend/src/pages/advertiser/CreateActivity.js
@@ -3,8 +3,7 @@ import ActivityForm from "../../components/advertiser/ActivityForm";
 import AdvertiserNavBar from "../../components/advertiser/AdvertiserNavBar";
 
 const CreateActivity = () => {
-    const advertiserId = "6701cb215e553adca0a5c62a";
-  
+    
     return (
       <div>
         <AdvertiserNavBar />
