3abdf4cb81185b11f4b2a783f8671b838f13ce4b
janasaad7 <janaahmedsaad@gmail.com>
2024-10-06 17:00:48 +0300

    create itineraries + tourguide navbars

--- frontend/src/pages/tourguide/TourguideHome.js
+++ frontend/src/pages/tourguide/TourguideHome.js
@@ -1,13 +1,10 @@
 import React from "react";
 import TourguideNavBar from "../../components/tourguide/TourguideNavBar";
-import ItinerariesForm from '../../components/tourguide/ItineraryForm.js';
 
 const TourguideHome = () => {
   return (
     <div>
         <TourguideNavBar />
-        <h1>Create your Itinerary</h1>
-        <ItinerariesForm />
     </div>
   );
 };
