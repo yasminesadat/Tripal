fe19669074c90067030a944988097cd87a6712bd
Mennah Shaker <mennahmohamedshaker0@gmail.com>
2024-10-07 08:36:24 +0300

    CRU seller done
    - register using username, email, password and choose seller
    - go to seller profile using a hardcoded ID :(
    - name and description should be N/A, when clicking on edit profile, update them along with the username, email, password

--- frontend/src/IDs.js
+++ frontend/src/IDs.js
@@ -1,4 +1,4 @@
-const sellerId = "66ffc04687831d849b4be0ec";
+const sellerId = "670305d50b8e82402b0852e7";
 const tourismGovernerID="66fe73625398ec9fd4651fa9";
 module.exports = {
   sellerId,
--- frontend/src/routes/SellerRoutes.js
+++ frontend/src/routes/SellerRoutes.js
@@ -1,7 +1,8 @@
 import SellerHome from "../pages/seller/SellerHome"
-
+import SellerProfile from "../components/seller/SellerProfile";
 const sellerRoutes = [
   { path: "/seller", element: <SellerHome /> },
+  { path: "/seller/profile", element: <SellerProfile /> },
 ];
 
 export default sellerRoutes;
