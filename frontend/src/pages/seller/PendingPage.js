3916863601eeeaa08e56e5108eb97d29a84ddfdf
hanaseif19 <hana.seiff19@gmail.com>
2024-10-11 13:20:43 +0300

    styles for pending page

--- frontend/src/pages/seller/PendingPage.js
+++ frontend/src/pages/seller/PendingPage.js
@@ -1,16 +1,14 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import { useLocation } from 'react-router-dom';
+import { useLocation } from "react-router-dom";
 
 const PendingPage = () => {
     const location = useLocation();
     return (
-        <div className="page-container">
-
+        <div className="pending-page-container">
             <h1>Your request is currently being processed, Wait for a notification</h1>
-            <p> User Name: {location.state.userName}</p>
-            <p> Email: {location.state.email}</p>
-            <p> Role: {location.state.role}</p>
+            <p>User Name: {location.state.userName}</p>
+            <p>Email: {location.state.email}</p>
+            <p>Role: {location.state.role}</p>
         </div>
     );
 };
