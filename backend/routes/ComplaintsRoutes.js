d414f5db71a76e07671198fdb0def1765486280d
YaraElkousy <128088959+YaraElkousy@users.noreply.github.com>
2024-10-13 16:12:55 +0300

    complaints

--- backend/routes/ComplaintsRoutes.js
+++ backend/routes/ComplaintsRoutes.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { createComplaint } = require('../controllers/ComplaintsController')
+const { createComplaint,getComplaintsByTourist,getAllComplaints,getComplaintById,updateComplaintStatus,replyToComplaint } = require('../controllers/ComplaintsController')
 
-router.post("/complaint/:id", createComplaint);
+router.post("/complaint/:id", createComplaint); //tourist id
+router.get('/complaints/tourist/:id', getComplaintsByTourist); //tourist id
+router.get('/complaints', getAllComplaints);
+router.get('/complaints/:id', getComplaintById); //complaint id
+router.put('/complaint/status/:id', updateComplaintStatus); //complaint id
+router.put('/complaint/reply/:id', replyToComplaint); //complaint id
+
+
+
+
 
 
 module.exports = router;
