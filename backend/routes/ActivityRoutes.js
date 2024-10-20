6d50f3789fce3d4a78799cd8046154b6714e376e
hanaseif19 <hana.seiff19@gmail.com>
2024-10-21 00:37:06 +0300

    fixing some imports

--- backend/routes/ActivityRoutes.js
+++ backend/routes/ActivityRoutes.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const {createActivity, getAdvertiserActivities, updateActivity, deleteActivity, viewUpcomingActivities} = require('../controllers/ActivityController');  
 const validateIDs = require("../middleware/IDMiddleware");
-const { addRating, getRatings } = require("../controllers/RatingController"); 
-const Activity = require("../models/Activity"); 
+const { addRating, getRatings } = require("../controllers/RatingController");
+const Activity = require("../models/Activity");
 const ActivityRating = require("../models/ActivityRating");
-const {createActivity, getAdvertiserActivities, updateActivity, deleteActivity, addRating, viewUpcomingActivities, addActivityComment, getActivityComments} = require('../controllers/ActivityController');  
+const { createActivity, getAdvertiserActivities, updateActivity, deleteActivity, addRating, viewUpcomingActivities, addActivityComment, getActivityComments } = require('../controllers/ActivityController');
 
 
-router.get('/activities/advertiser/:id', getAdvertiserActivities); 
-router.post('/activities', createActivity);    
-router.put('/activities/:id', updateActivity); 
-router.delete('/activities/:id', deleteActivity); 
+router.get('/activities/advertiser/:id', getAdvertiserActivities);
+router.post('/activities', createActivity);
+router.put('/activities/:id', updateActivity);
+router.delete('/activities/:id', deleteActivity);
 router.get('/activities/view', viewUpcomingActivities);
 router.post("/activities/:id/ratings", validateIDs(["id", "userID"]), addRating(Activity, ActivityRating, 'activityID'));
 router.get("/activities/:id/ratings", validateIDs(["id"]), getRatings(Activity, ActivityRating, 'activityID'));
