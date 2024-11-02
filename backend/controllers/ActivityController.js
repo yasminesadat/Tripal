a755bad9e37a24a77405b4ba4e64a386bb551148
diana <daiana.rehan.dr@gmail.com>
2024-11-02 18:35:31 +0200

    checking for the age before booking an activity

--- backend/controllers/ActivityController.js
+++ backend/controllers/ActivityController.js
@@ -176,12 +176,15 @@ const bookActivity = async (req, res) => {
       return res.status(400).json({ error: 'Booking is closed for this activity' });
     }
     const tourist = await Tourist.findById(touristId);
-    if (!tourist) {
+    if (!tourist)
       return res.status(404).json({ error: 'Tourist not found' });
-    }
-    if (activity.tourists.includes(touristId)) {
+    
+    if(tourist.calculateAge() < 18) 
+      return res.status(403).json({ error: 'You must be at least 18 years old to book an activity' }); 
+
+    if (activity.tourists.includes(touristId)) 
       return res.status(400).json({ error: 'You have already booked this activity.' });
-    }
+    
     activity.tourists.push(touristId);
     await activity.save();
     res.status(200).json({ message: 'Activity booked successfully' });
