7bbc7a1a2b10331a2abdeb70565316266d507389
hanaseif19 <hana.seiff19@gmail.com>
2024-11-04 15:50:08 +0200

    fixed id of tour guide (TAKE CARE!)

--- frontend/src/IDs.js
+++ frontend/src/IDs.js
@@ -3,7 +3,7 @@ const tourismGovernerID = "6708508701900d8424372bb6";
 const adminId = "670c3f8b4d912d42ccf5580c";
 const touristId = "6724842b5831eed787083b57"; // DONT DELETE
 const advertiserID = "6726aaea44ae09c9c4e1f809"; // advertiser profile final NEVER DELETE
-const tourguideID="672678ed528c25edc7fc0f55";
+const tourGuideID = "672678ed528c25edc7fc0f55";
 //mimics what will be in token to adjust some component
 // features based on it
 //change to test different roles in the website
@@ -18,5 +18,5 @@ module.exports = {
   adminId,
   touristId,
   advertiserID,
-  tourguideID
+  tourGuideID
 };
