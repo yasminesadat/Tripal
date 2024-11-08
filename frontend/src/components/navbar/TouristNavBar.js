1c5bf2b22f977eb9894c310678476e7312b40c2c
hanaseif19 <hana.seiff19@gmail.com>
2024-11-08 18:44:32 +0200

    fixed navbar , added space for hotel and flights booking,

--- frontend/src/components/navbar/TouristNavBar.js
+++ frontend/src/components/navbar/TouristNavBar.js
@@ -9,42 +9,59 @@ const TouristNavBar = () => {
         <div class="nav-bar-link">
           <Link to="/tourist">Home</Link>
         </div>
-        <div class="nav-bar-link">
-          <Link to={`/tourist/profile`}>Profile</Link>
+
+
+        <div className="nav-bar-link dropdown">
+          <button className="dropbtn">Activities</button>
+          <div className="dropdown-content">
+            <Link to="/activities-history">Activities History</Link>
+            <Link to="/booked-activities">Booked Activities</Link>
+            <Link to="/upcoming-activities">Upcoming Activities</Link>          </div>
         </div>
-        <div class="nav-bar-link">
-          <Link to="/upcoming-activities">Upcoming Activities</Link>
+        <div className="nav-bar-link dropdown">
+          <button className="dropbtn">Itineraries</button>
+          <div className="dropdown-content">
+            <Link to="/itineraries-history">Itineraries History</Link>
+            <Link to="/upcoming-itineraries">Upcoming Itineraries</Link>
+            <Link to="/itineraries/booked-itineraries">Booked Itineraries</Link>
+          </div>
         </div>
-        <div class="nav-bar-link">
-          <Link to="/booked-activities">Booked Activities</Link>
-        </div>
-        <div class="nav-bar-link">
-          <Link to="/activities-history">Activities History</Link>
-        </div>
-        <div class="nav-bar-link">
-          <Link to="/upcoming-itineraries">Upcoming Itineraries</Link>
-        </div>
-        <div class="nav-bar-link">
-          <Link to="/itineraries/booked-itineraries">Booked Itineraries</Link>
-        </div>
-        <div class="nav-bar-link">
-          <Link to="/itineraries-history">Itineraries History</Link>
-        </div>
+
+
         <div class="nav-bar-link">
           <Link to="/historical-places">Historical Places</Link>
         </div>
         <div class="nav-bar-link">
           <Link to="/tourist/view-products">Products</Link>
         </div>
-        <div class="nav-bar-link">
-          <Link to={`/tourist/create-complaint`}>Create Complaint</Link>
+        <div className="nav-bar-link dropdown">
+          <button className="dropbtn">Complaints </button>
+          <div className="dropdown-content">
+            <Link to={`/tourist/create-complaint`}>Create Complaint</Link>
+            <Link to={`/tourist/view-Complaints`}>View Complaints</Link>
+
+          </div>
         </div>
-        <div class="nav-bar-link">
-          <Link to={`/tourist/view-Complaints`}>View Complaints</Link>
+
+        <div className="nav-bar-link dropdown">
+          <button className="dropbtn">Profile Management</button>
+          <div className="dropdown-content">
+            <Link to={`/tourist/profile`}>Profile</Link>
+            <Link to="/">Log out</Link>
+
+          </div>
         </div>
-        <div class="nav-bar-link">
-          <Link to="/">Log out</Link>
+
+        <div className="nav-bar-link dropdown">
+          <button className="dropbtn">Hotel & Flights </button>
+          <div className="dropdown-content">
+            <Link to="" >Book Hotel</Link>
+            <Link to="" >Book Flights</Link>
+            <Link to="">History</Link>
+
+          </div>
         </div>
+
       </div>
     </nav >
   );
