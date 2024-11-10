86462dbbfcc8f553ed754a4cc6357a7cf1cedbb7
YaraElkousy <128088959+YaraElkousy@users.noreply.github.com>
2024-11-10 18:04:05 +0200

    activities optimization fix

--- frontend/src/pages/commonPagesForMultipleUsers/Activities.js
+++ frontend/src/pages/commonPagesForMultipleUsers/Activities.js
@@ -172,7 +172,7 @@ const Activities = ({ isAdvertiser, isTourist }) => {
              />
         </div>
         :
-        <AdvertiserActivities />
+        <AdvertiserActivities activities={activities} />
       }
 
       <Footer />
