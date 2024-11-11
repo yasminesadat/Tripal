d3abe5273e0377b8a0f5bab94d7c5d76447340c4
hanaseif19 <hana.seiff19@gmail.com>
2024-11-12 00:27:28 +0200

    Revert "showing admin/anon instead of the id +time"
    This reverts commit c2ffc5deb4920cedeed74be1160359264b4d6f7b.

--- frontend/src/pages/tourist/MyComplaints.js
+++ frontend/src/pages/tourist/MyComplaints.js
@@ -122,11 +122,8 @@ const MyComplaints = () => {
                                                             <h4>Replies</h4>
                                                             <ul>
                                                                 {selectedComplaint.replies.map((reply, index) => (
-                                                                    <li className="text-14 bg-light-1 rounded-12 py-20 px-30 mt-15" key={index}>
-                                                                    {reply.message} (from: Anonymous) on {new Intl.DateTimeFormat('en-US', {
-                                                                        weekday: 'short', year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric'
-                                                                    }).format(new Date(reply.date))}
-                                                                    </li>                                                                ))}
+                                                                    <li class="text-14 bg-light-1 rounded-12 py-20 px-30 mt-15" key={index}>{reply.message} (from: {reply.senderId}) on {new Date(reply.date).toLocaleDateString()}</li>
+                                                                ))}
                                                             </ul>
 
                                                             {/* Reply Form */}
