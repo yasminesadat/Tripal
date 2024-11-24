712c99c56c070cce54322aff2715b1864e989436
hanaseif19 <hana.seiff19@gmail.com>
2024-11-24 16:23:14 +0200

    tourist profile added the data in template(can now view) + added assets folder

--- frontend/src/pages/tourist/TouristProfile.jsx
+++ frontend/src/pages/tourist/TouristProfile.jsx
@@ -1,26 +1,27 @@
-import React, { useEffect, useState } from "react";
+import Sidebar from "@/components/dasboard/Sidebar";
+import Header from "@/components/dasboard/Header";
+import { useState, useEffect } from "react";
 import { getTouristInformation, updateTouristInformation, redeemPoints } from "../../api/TouristService";
-// import TouristNavBar from "../../components/navbar/TouristNavBar";
 import { useNavigate, useParams } from "react-router-dom";
-import { nationalities } from "../../assets/Nationalities";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css"; 
-import Badge from "../../components/tourist/Badge";
+import { nationalities } from "@/assets/Nationalities";
+import Badge from "@/components/tourist/Badge";
 import Currency from "../../components/tourist/Currency";
-import ChangePassword from "../../components/common/ChangePassword";
+// import ChangePassword from "../../components/common/ChangePassword";
 import { requestAccountDeletion } from "../../api/RequestService";
 import { message } from 'antd';
-// import { touristId } from "../../IDs";
 import { getTouristTags } from "../../api/TouristService";
 import { getTouristCategories } from "../../api/TouristService";
 import { getTags } from "../../api/PreferenceTagService";
 import ActivityCategoryService from "../../api/ActivityCategoryService";
 import { Select } from 'antd';
 import { Tag } from 'antd';
+export default function Profile() {
+  const [sideBarOpen, setSideBarOpen] = useState(false);
+  const [image1, setImage1] = useState("");
+  const [image2, setImage2] = useState("/img/dashboard/addtour/1.jpg");
 
-const TouristHomePage = () => {
-  const id = touristId;
-  const userType = "tourist";
+
+
   const [profileInformation, setProfileInformation] = useState({});
   const [isEditing, setIsEditing] = useState(false);
   const [touristTags, setTouristTags] = useState([]);
@@ -28,8 +29,6 @@ const TouristHomePage = () => {
   const [allTags, setAllTags] = useState([]);
   const [allCats, setAllCats] = useState([]);
   const navigate = useNavigate();
-
-
 
   const [editedProfile, setEditedProfile] = useState({
     email: "",
@@ -69,7 +68,6 @@ const TouristHomePage = () => {
       console.log("Editing mode enabled");
     }
   };
-
   const handleTagsChange = (value) => {
     setEditedProfile((prevState) => ({
       ...prevState,
@@ -83,10 +81,9 @@ const TouristHomePage = () => {
       categories: value,
     }));
   };
-
   const getUserInformation = async () => {
     try {
-      const response = await getTouristInformation(id);
+      const response = await getTouristInformation();
       setProfileInformation(response);
       setEditedProfile({
         email: response.email,
@@ -102,7 +99,6 @@ const TouristHomePage = () => {
       console.error("Failed to fetch user information:", error);
     }
   };
-
   const fetchTouristTags = async () => {
     try {
       const tags = await getTouristTags(id);
@@ -174,7 +170,7 @@ const TouristHomePage = () => {
 
   const handleDeletion = async () => {
     try {
-      const response = await requestAccountDeletion();
+      const response = await requestAccountDeletion("Tourist", id);
       message.success(response.message);
       navigate("/");
     } catch (error) {
@@ -189,206 +185,292 @@ const TouristHomePage = () => {
     fetchAllTags();
     fetchAllCategories();
   }, []);
+  const handleImageChange = (event, func) => {
+    const file = event.target.files[0];
 
+    if (file) {
+      const reader = new FileReader();
 
+      reader.onloadend = () => {
+        func(reader.result);
+      };
+
+      reader.readAsDataURL(file);
+    }
+  };
   return (
-    <div>
-      {/* <TouristNavBar /> */}
-      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-        <h1>Your Profile</h1>
-        <div>
-          {profileInformation.wallet && profileInformation.wallet.currency && (
-            <Currency userCurrency={profileInformation.choosenCurrency} onCurrencyChange={handleCurrencyChange} />
-          )}
-          {profileInformation.totalPoints !== undefined && (
-            <Badge totalPoints={profileInformation.totalPoints} />
-          )}
+    <>
+
+      <div
+        className={`dashboard ${sideBarOpen ? "-is-sidebar-visible" : ""
+          } js-dashboard`}
+      >
+
+        {profileInformation.totalPoints !== undefined && (
+          <Badge totalPoints={profileInformation.totalPoints} />
+        )}
+        <Sidebar setSideBarOpen={setSideBarOpen} />
+
+        <div className="dashboard__content">
+
+          <div className="dashboard__content_content">
+            <h1 className="text-30">Tourist profile</h1>
+            <p className="">{profileInformation.userName}</p>
+            <div className="mt-50 rounded-12 bg-white shadow-2 px-40 pt-40 pb-30">
+              <h5 className="text-20 fw-500 mb-30">Profile Details</h5>
+
+              <div className="contactForm row y-gap-30">
+                <div className="col-md-6">
+                  <div className="form-input ">
+                    <input
+                      type="text"
+                      name="userName"
+                      value={profileInformation.userName}
+
+                    />
+                    <label className="lh-1 text-16 text-light-1">Username</label>
+                  </div>
+                </div>
+
+                <div className="col-md-6">
+                  <div className="form-input ">
+                    <input
+                      type="text"
+                      name="email"
+                      value={editedProfile.email}
+                      onChange={handleInputChange} // Handle input changes
+
+                    />
+                    <label className="lh-1 text-16 text-light-1">
+                      Email
+                    </label>
+                  </div>
+                </div>
+
+                <div className="col-md-6">
+                  <div className="form-input ">
+                    <input
+                      type="text"
+                      name="dateOfBirth"
+                      value={new Date(
+                        profileInformation.dateOfBirth
+                      ).toLocaleDateString()}
+
+                    />
+                    <label className="lh-1 text-16 text-light-1">Date of Birth</label>
+                  </div>
+                </div>
+
+                <div className="col-md-6">
+                  <div className="form-input ">
+                    <input
+                      type="text"
+                      name="mobileNumber"
+                      value={editedProfile.mobileNumber}
+                      onChange={handleInputChange}
+                    />
+                    <label className="lh-1 text-16 text-light-1">Phone Number</label>
+                  </div>
+                </div>
+
+                <div className="col-md-6">
+                  <div className="form-input ">
+                    {profileInformation.wallet ? (
+                      <input
+                        type="text"
+                        name="walletBalance"
+                        value={
+                          profileInformation.wallet.amount +
+                          " " +
+                          profileInformation.wallet.currency
+                        }
+                        readOnly
+                      />
+                    ) : (
+                      <span>No wallet information available</span>
+                    )}
+                    <label className="lh-1 text-16 text-light-1">Wallet</label>
+                  </div>
+                </div>
+
+                <div className="col-md-6">
+                  <div className="form-input">
+                    {profileInformation.currentPoints !== undefined ? (
+                      <input
+                        type="text"
+                        name="currentPoints"
+                        value={profileInformation.currentPoints} // Corrected value assignment
+                        readOnly
+                      />
+                    ) : (
+                      <span>No points information available</span>
+                    )}
+                    <label className="lh-1 text-16 text-light-1">Total Points</label>
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <div className="form-input ">
+                    <input
+                      type="text"
+                      name="job"
+                      value={editedProfile.job}
+                      onChange={handleInputChange}
+
+                    />
+                    <label className="lh-1 text-16 text-light-1">Job</label>
+                  </div>
+                </div>
+
+                <div className="col-md-6">
+                  <div className="form-input ">
+                    <input
+                      type="text"
+                      name="nationality"
+                      value={editedProfile.nationality}
+                      onChange={handleInputChange}
+
+                    />
+                    <label className="lh-1 text-16 text-light-1">Nationality</label>
+                  </div>
+                </div>
+
+                <p>
+                  <b>Chosen Preference Tags:</b>
+                </p>
+
+                <Select
+                  mode="multiple"
+                  value={editedProfile.tags}
+                  onChange={handleTagsChange}
+                  placeholder="Select tags"
+                  style={{ width: '100%' }}
+                >
+                  {allTags.map((tag) => (
+                    <Select.Option key={tag._id} value={tag._id}>
+                      {tag.name}
+                    </Select.Option>
+                  ))}
+                </Select>
+                <p>
+                  <b>Chosen Activity Categories:</b>
+                </p>
+
+                <Select
+                  mode="multiple"
+                  value={editedProfile.categories}
+                  onChange={handleCategoriesChange}
+                  placeholder="Select Categories"
+                  style={{ width: '100%' }}
+                >
+                  {allCats.map((category) => (
+                    <Select.Option key={category._id} value={category._id}>
+                      {category.Name}
+                    </Select.Option>
+                  ))}
+                </Select>
+
+
+
+                <div className="col-12">
+                  <button className="button -md -dark-1 bg-accent-1 text-white mt-30">
+                    Save Changes
+                    <i className="icon-arrow-top-right text-16 ml-10"></i>
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            <div className="rounded-12 bg-white shadow-2 px-40 pt-40 pb-30 mt-30">
+              <h5 className="text-20 fw-500 mb-30">Change Password</h5>
+
+              <div className="contactForm y-gap-30">
+                <div className="row y-gap-30">
+                  <div className="col-md-6">
+                    <div className="form-input ">
+                      <input type="text" required />
+                      <label className="lh-1 text-16 text-light-1">
+                        Old password
+                      </label>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="form-input ">
+                      <input type="text" required />
+                      <label className="lh-1 text-16 text-light-1">
+                        New password
+                      </label>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="form-input ">
+                      <input type="text" required />
+                      <label className="lh-1 text-16 text-light-1">
+                        Confirm new password
+                      </label>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="row">
+                  <div className="col-12">
+                    <button className="button -md -dark-1 bg-accent-1 text-white">
+                      Save Changes
+                      <i className="icon-arrow-top-right text-16 ml-10"></i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="text-center pt-30">
+              © Copyright Tripal {new Date().getFullYear()}
+            </div>
+          </div>
         </div>
       </div>
-      <div>
-        <ul className="tourist-profile">
-          <li key={profileInformation._id}>
-            <h2>
-              <input
-                type="text"
-                name="userName"
-                value={profileInformation.userName}
-                readOnly={!isEditing} // Allow editing if in editing mode
-              />
-            </h2>
-            <p>
-              <b>Email:</b>
-              <input
-                type="text"
-                name="email"
-                value={editedProfile.email}
-                onChange={handleInputChange} // Handle input changes
-                readOnly={!isEditing}
-              />
-            </p>
-            <p>
-              <b>Date of birth:</b>
-              <input
-                type="text"
-                name="dateOfBirth"
-                value={new Date(
-                  profileInformation.dateOfBirth
-                ).toLocaleDateString()}
-                readOnly
-              />
-            </p>
-            <p>
-              <b>Nationality:</b>
-              {isEditing ? (
-                <select
-                  name="nationality"
-                  value={editedProfile.nationality}
-                  onChange={handleInputChange} // Handle changes
-                >
-                  {nationalities.map((nationality) => (
-                    <option key={nationality} value={nationality}>
-                      {nationality}
-                    </option>
-                  ))}
-                </select>
-              ) : (
-                <input type="text" value={editedProfile.nationality} readOnly />
-              )}
-            </p>
-
-            <p>
-              <b>Chosen Preference Tags:</b>
-            </p>
-
-            {isEditing ? (
-              <Select
-                mode="multiple"
-                value={editedProfile.tags}
-                onChange={handleTagsChange}
-                placeholder="Select tags"
-                style={{ width: '100%' }}
-              >
-                {allTags.map((tag) => (
-                  <Select.Option key={tag._id} value={tag._id}>
-                    {tag.name}
-                  </Select.Option>
-                ))}
-              </Select>
-            ) : (
-
-              touristTags.length > 0 ? (
-                <input
-                  type="text"
-                  value={touristTags.map((tag) => tag.name).join(", ")}
-                  readOnly
-                />
-              ) : (
-                <span>No chosen preference tags</span>
-              )
-
-            )}
-
-            <p>
-              <b>Chosen Actvity Categories:</b>
-            </p>
-
-            {isEditing ? (
-              <Select
-                mode="multiple"
-                value={editedProfile.categories}
-                onChange={handleCategoriesChange}
-                placeholder="Select Categories"
-                style={{ width: '100%' }}
-              >
-                {allCats.map((category) => (
-                  <Select.Option key={category._id} value={category._id}>
-                    {category.Name}
-                  </Select.Option>
-                ))}
-              </Select>
-            ) : (
-
-              touristCategories.length > 0 ? (
-                <input
-                  type="text"
-                  value={touristCategories.map((category) => category.Name).join(", ")}
-                  readOnly
-                />
-              ) : (
-                <span>No chosen activity categories</span>
-              )
-            )}
-
-            <p>
-              <b>Job:</b>
-              <input
-                type="text"
-                name="job"
-                value={editedProfile.job}
-                onChange={handleInputChange}
-                readOnly={!isEditing}
-              />
-            </p>
-            <p>
-              <b>Mobile Number:</b>
-              <input
-                type="text"
-                name="mobileNumber"
-                value={editedProfile.mobileNumber}
-                onChange={handleInputChange}
-                readOnly={!isEditing}
-              />
-            </p>
-
-            <p>
-              <b>Balance:</b>
-              {profileInformation.wallet ? (
-                <input
-                  type="text"
-                  name="walletBalance"
-                  value={
-                    profileInformation.wallet.amount +
-                    " " +
-                    profileInformation.wallet.currency
-                  }
-                  readOnly
-                />
-              ) : (
-                <span>No wallet information available</span>
-              )}
-            </p>
-            <p>
-              <b>Points:</b>
-              {profileInformation.currentPoints !== undefined ? (
-                <>
-                  <input
-                    type="text"
-                    name="currentPoints"
-                    value={profileInformation.currentPoints}
-                    readOnly
-                  />
-                  <button onClick={handleRedeemClick} style={{ marginLeft: '10px' }}>
-                    Redeem points to cash
-                  </button>
-                </>
-              ) : (
-                <span>No points</span>
-              )}
-            </p>
+      <style>
+        {`
+        .contactForm .form-input {
+          position: relative;
+        margin-bottom: 20px;
+          }
+        .contactForm .form-input label {
+          position: absolute;
+        left: 10px;
+        top: 50%;
+        transform: translateY(-50%);
+        transition: 0.3s ease;
+        color: #aaa;
+          }
+        .contactForm .form-input input:focus + label,
+        .contactForm .form-input textarea:focus + label,
+        .contactForm .form-input input:not(:placeholder-shown) + label,
+        .contactForm .form-input textarea:not(:placeholder-shown) + label,
+        .contactForm .form-input input.filled + label,
+        .contactForm .form-input textarea.filled + label {
+          transform: translateY(-29px);
+        font-size: 12px;
+        color: #333;
+          }
+        .contactForm .form-input input,
+        .contactForm .form-input textarea {
+          padding: 10px;
+        font-size: 16px;
+        width: 100%;
+        border: 1px solid #ccc;
+        outline: none;
+          }
+          `}
+      </style>
+    </>
 
 
 
-          </li>
-        </ul>
-        <button onClick={handleEditClick}>
-          {isEditing ? "Save" : "Update"}
-        </button>
-        <button onClick={handleDeletion}>Delete Account</button>
-      </div>
-      <ToastContainer />
-      <ChangePassword id={id} userType={userType} />
-    </div>
   );
-};
-
-export default TouristHomePage;
+}
