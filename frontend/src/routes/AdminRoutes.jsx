03b54cce5d9bc6ff53b782a296e886e8fd67fd23
MernaSenger <mernasenger753@gmail.com>
2024-11-25 03:49:12 +0200

    add admin and governor

--- frontend/src/routes/AdminRoutes.jsx
+++ frontend/src/routes/AdminRoutes.jsx
@@ -4,8 +4,8 @@ import ActivityCategoryDetails from "@/pages/admin/ActivityCategory";
 // import AdminProducts from "../pages/admin/AdminProducts";
 // import AdminViewProduct from "../pages/admin/AdminViewProduct";
 import AdminViewUsers from "../pages/admin/AdminViewUsers";
-// import CreateNewGovernor from "../pages/admin/CreateNewGovernor";
-// import CreateNewAdmin from "../pages/admin/CreateNewAdmin";
+import CreateNewGovernor from "@/pages/governor/CreateNewGovernor";
+import CreateNewAdmin from "@/pages/admin/CreateNewAdmin";
 import ComplaintsPage from "@/pages/admin/Complaints";
 import ComplaintsReplies from "@/pages/admin/ComplaintsReplies";
 import TagManager from "@/pages/admin/PreferenceTags";
@@ -24,8 +24,8 @@ const adminRoutes = [
   // { path: "/admin/view-products", element: <AdminProducts /> },
   // { path: "/admin/view-products/product/:id", element: <AdminViewProduct /> },
   { path: "/admin/delete-user", element: <AdminViewUsers /> },
-  // { path: "/admin/new-governor", element: <CreateNewGovernor /> },
-  // { path: "/admin/new-admin", element: <CreateNewAdmin /> },
+  { path: "/admin/new-governor", element: <CreateNewGovernor /> },
+  { path: "/admin/new-admin", element: <CreateNewAdmin /> },
   { path: "/admin/complaints", element: <ComplaintsPage /> },
   { path: "/admin/complaints/replies", element: <ComplaintsReplies /> },
   { path: "/revenue", element: <Revenue /> },
