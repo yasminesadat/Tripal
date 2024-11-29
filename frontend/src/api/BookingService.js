fda976d2b0e4b153618813e418c1dfe903aad56b
diana <daiana.rehan.dr@gmail.com>
2024-11-29 18:42:02 +0200

    fixing booking/cancelation to use userId without sending it via frontend

--- frontend/src/api/BookingService.js
+++ frontend/src/api/BookingService.js
@@ -1,11 +1,8 @@
 import { axios } from "./axios";
 
-export async function bookResource(resourceType, resourceId, touristId,tickets) {
+export async function bookResource(resourceType, resourceId,tickets) {
   try {
-    const response = await axios.post(`/${resourceType}/${resourceId}/book`, { 
-      touristId,
-      tickets
-    });
+    const response = await axios.post(`/${resourceType}/${resourceId}/book`, { tickets});
     return response.data;
   } catch (error) {
     console.error(`Error booking ${resourceType}:`, error);
@@ -14,9 +11,9 @@ export async function bookResource(resourceType, resourceId, touristId,tickets) 
 }
 
 
-export async function cancelResource(resourceType, resourceId, touristId) {
+export async function cancelResource(resourceType, resourceId) {
   try {
-    const response = await axios.post(`/${resourceType}/${resourceId}/cancel`, { touristId });
+    const response = await axios.post(`/${resourceType}/${resourceId}/cancel`);
     return response.data;
   } catch (error) {
     console.error(`Error canceling ${resourceType} booking:`, error);
