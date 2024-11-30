2e0ad00e7fa995c2bd479fc447de8d1adf171819
hanaseif19 <hana.seiff19@gmail.com>
2024-11-30 02:26:22 +0200

    added meta component to the pending requests page

--- frontend/src/pages/commonPagesForMultipleUsers/PendingRequest.jsx
+++ frontend/src/pages/commonPagesForMultipleUsers/PendingRequest.jsx
@@ -4,11 +4,18 @@ import { Card, Timeline } from 'antd';
 import { CheckCircleOutlined, SyncOutlined, ClockCircleOutlined } from '@ant-design/icons';
 import FooterThree from "@/components/layout/footers/FooterThree";
 import GuestHeader from "@/components/layout/header/GuestHeader";
+import MetaComponent from "@/components/common/MetaComponent";
 const PendingPage = () => {
     const location = useLocation();
 
+
+    const metadata = {
+        title: 'Request Processing | Tripal',
+        description: 'Your application is being reviewed by our team'
+    }
     return (
         <>
+            <MetaComponent meta={metadata} />
 
             <div className="page-wrapper">
                 <GuestHeader />
