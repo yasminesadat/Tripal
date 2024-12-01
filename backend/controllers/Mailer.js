806abb5c8cac668b39bfcbfee2ebf508f66f7277
diana <daiana.rehan.dr@gmail.com>
2024-12-01 12:05:10 +0200

    first test sending an email (worked)

--- backend/controllers/Mailer.js
+++ backend/controllers/Mailer.js
@@ -1,15 +1,31 @@
-/* global process */
 const nodemailer = require('nodemailer');
 const dotenv = require('dotenv');
 dotenv.config(); 
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',   
-  secure: false,
+  service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  logger: true,
+  debug: true,
+});
+
+
+const mailOptions = {
+  from: process.env.EMAIL_USER, 
+  to: 'daiana.rehan.dr@gmail.com',
+  subject: 'Test Email from Node.js',
+  text: 'Hello, this is a test email from Node.js!',
+};
+
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+      console.log('Error:', error);
+  } else {
+      console.log('Email sent:', info.response);
+  }
 });
 
 module.exports = transporter;
