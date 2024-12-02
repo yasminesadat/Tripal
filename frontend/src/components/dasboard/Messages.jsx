0d222662992c542e06d9156bf4ac24fb733a4def
hanaseif19 <hana.seiff19@gmail.com>
2024-12-03 00:26:23 +0200

    removed copyright thing at the bottom

--- frontend/src/components/dasboard/Messages.jsx
+++ frontend/src/components/dasboard/Messages.jsx
@@ -253,9 +253,7 @@ export default function Messages({ complaint, user, role }) {
 
             </div>
 
-            <div className="text-center pt-30">
-              © Copyright Tripal {new Date().getFullYear()}
-            </div>
+
           </div>
         </div>
       </div>
