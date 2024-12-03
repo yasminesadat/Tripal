95149d52b8e4a8c80e606431a2a5e0152400d66e
hanaseif19 <hana.seiff19@gmail.com>
2024-12-03 21:35:18 +0200

    seller create product styled text boxes

--- frontend/src/components/activity/activitySingle/TourSingleSidebar.jsx
+++ frontend/src/components/activity/activitySingle/TourSingleSidebar.jsx
@@ -1,11 +1,11 @@
-import  { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef } from "react";
 import { getConversionRate, getTouristCurrency } from "@/api/ExchangeRatesService";
 import { message } from "antd";
 import { getUserData } from "@/api/UserService";
 import { bookResource } from "@/api/BookingService";
 
-export default function TourSingleSidebar({itinerary, activity, refActivityBook, refItineraryBook}) {
-  const [userRole, setUserRole] = useState(null); 
+export default function TourSingleSidebar({ itinerary, activity, refActivityBook, refItineraryBook }) {
+  const [userRole, setUserRole] = useState(null);
   const [exchangeRate, setExchangeRate] = useState(1);
   const [currency, setCurrency] = useState("EGP");
   const selectedRef = refActivityBook || refItineraryBook;
@@ -17,7 +17,7 @@ export default function TourSingleSidebar({itinerary, activity, refActivityBook,
         if (response.data.status === "success") {
           setUserRole(response.data.role);
         } else {
-          message.error(response.data.message); 
+          message.error(response.data.message);
         }
       } catch (error) {
         message.error("Failed to fetch user data.");
@@ -30,8 +30,8 @@ export default function TourSingleSidebar({itinerary, activity, refActivityBook,
     const intervalId = setInterval(async () => {
       const curr = getTouristCurrency();
       if (curr) {
-        setCurrency(curr); 
-        fetchExchangeRate(curr); 
+        setCurrency(curr);
+        fetchExchangeRate(curr);
       }
     }, 500); // every 500ms
 
@@ -54,9 +54,9 @@ export default function TourSingleSidebar({itinerary, activity, refActivityBook,
   };
 
   const [ticketNumber, setTicketCount] = useState(1);
-  
+
   const handleBookClick = async () => {
-      try {
+    try {
       const response = await bookResource(
         itinerary ? "itinerary" : "activity",
         itinerary ? itinerary._id : activity._id,
@@ -68,7 +68,7 @@ export default function TourSingleSidebar({itinerary, activity, refActivityBook,
       message.error(error.response?.data?.error || "Booking failed");
     }
   };
-  
+
   return (
     <div className="tourSingleSidebar">
       <h5 className="text-18 fw-500 mb-20 mt-20">Tickets</h5>
@@ -77,12 +77,12 @@ export default function TourSingleSidebar({itinerary, activity, refActivityBook,
         <div className="d-flex items-center justify-between">
           <div className="text-14">
             Tickets: {" "}
-            {itinerary&&<span className="fw-500">
-              {ticketNumber} x {formatPrice(itinerary.price.toFixed(2))} 
+            {itinerary && <span className="fw-500">
+              {ticketNumber} x {formatPrice(itinerary.price.toFixed(2))}
             </span>}
 
-            {activity&&<span className="fw-500">
-              {ticketNumber} x {formatPrice(activity.price.toFixed(2))} 
+            {activity && <span className="fw-500">
+              {ticketNumber} x {formatPrice(activity.price.toFixed(2))}
             </span>}
           </div>
 
@@ -157,29 +157,29 @@ export default function TourSingleSidebar({itinerary, activity, refActivityBook,
         <div className="text-14">$40</div>
       </div> */}
 
-      <div className="line mt-20 mb-20"/>
-        
+      <div className="line mt-20 mb-20" />
 
-        {itinerary&&<div className="text-14 ">
-        <span className="fw-500">Service Fee:  </span> 
-         {(formatPrice(itinerary.serviceFee.toFixed(2)))}
-        </div>}
+
+      {itinerary && <div className="text-14 ">
+        <span className="fw-500">Service Fee:  </span>
+        {(formatPrice(itinerary.serviceFee.toFixed(2)))}
+      </div>}
 
       <div className="d-flex items-center justify-between">
 
         <div className="text-18 fw-500">Total:</div>
-        {itinerary&&<div className="text-18 fw-500">
+        {itinerary && <div className="text-18 fw-500">
           {
-            formatPrice((itinerary.price * ticketNumber +itinerary.serviceFee).toFixed(2))}
+            formatPrice((itinerary.price * ticketNumber + itinerary.serviceFee).toFixed(2))}
         </div>}
 
-        {activity&&<div className="text-18 fw-500">
+        {activity && <div className="text-18 fw-500">
           {formatPrice((activity.price * ticketNumber).toFixed(2))}
         </div>}
 
       </div>
 
-      <button ref={selectedRef} onClick={handleBookClick}  className="button -md -dark-1 col-12 bg-accent-1 text-white mt-20">
+      <button ref={selectedRef} onClick={handleBookClick} className="button -md -dark-1 col-12 bg-accent-1 text-white mt-20">
         Book Now
         <i className="icon-arrow-top-right ml-10"></i>
       </button>
