6d36f4f0fc3c96c0c737c101bcb6311d4dfd655c
hanaseif19 <hana.seiff19@gmail.com>
2024-12-03 20:05:03 +0200

    itinerary form styling

--- frontend/src/pages/tourguide/ItineraryCreation.jsx
+++ frontend/src/pages/tourguide/ItineraryCreation.jsx
@@ -5,13 +5,13 @@ import CreateItineraryForm from "@/components/itinerary/CreateItineraryForm";
 const ItineraryCreationPage = () => {
     return (
         <div className="page-wrapper">
-        <TourGuideHeader />
-        <main className="page-content">
-            <div className="admin-content-details">
-            <CreateItineraryForm/>
-            </div>
-        </main>
-        <FooterThree />
+            <TourGuideHeader />
+            <main className="page-content-hana">
+                <div className="admin-content-details">
+                    <CreateItineraryForm />
+                </div>
+            </main>
+            <FooterThree />
         </div>
     );
 }
